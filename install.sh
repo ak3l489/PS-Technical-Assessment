83960a53d537331e026dc78e4f8f7eacb060ffd4
AK3L4 <65798312+ak3l489@users.noreply.github.com>
2023-11-21 21:19:11 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,5 +18,5 @@ pip3 install jupyter
 deactivate
 
 # Provide user instructions
-echo "Environment setup complete.
+echo "Environment setup complete. To activate the virtual environment, use 'source venv/bin/activate'."
 echo "To run Jupyter Notebook, use the command 'jupyter notebook'."
